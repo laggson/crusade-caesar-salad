fb949db5f6a00c27fea49492fe567e116e056bee
Laggson <m.estrich98@gmail.com>
2016-08-28 17:25:16 +0200

    Got the base working

---
+++ WCFChat.Logic/Control.cs
@@ -0,0 +1,14 @@
+﻿using WCFChat.Logic.Server;
+
+namespace WCFChat.Logic
+{
+    public class Control
+    {
+        public string Test(int value)
+        {
+            Service1Client client = new Service1Client();
+
+            return client.GetData(value);
+        }
+    }
+}
